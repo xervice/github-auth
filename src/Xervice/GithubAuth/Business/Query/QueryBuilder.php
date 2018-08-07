27f08d868a312aa79919c670d0a6df2627ca7685
Mike Bertram <contact@mibexx.de>
2018-08-07 08:40:10 +0200

    * Bugfix QueryBuilder

--- src/Xervice/GithubAuth/Business/Query/QueryBuilder.php
+++ src/Xervice/GithubAuth/Business/Query/QueryBuilder.php
@@ -27,16 +27,17 @@ class QueryBuilder implements QueryBuilderInterface
     public function appendParams(array $params): void
     {
         $urlData = parse_url($this->url);
-        parse_str($urlData['query'], $queryData);
-
-        $queryData = array_merge($queryData, $params);
-
+        if (!isset($urlData['query'])) {
+            parse_str($urlData['query'], $queryData);
+            $queryData = array_merge($queryData, $params);
+        } else {
+            $queryData = $params;
+        }
         $urlData['query'] = http_build_query($queryData);
 
         if (function_exists('http_build_url')) {
             $this->url = http_build_url($urlData);
-        }
-        else {
+        } else {
             $this->url = sprintf(
                 '%s://%s%s?%s',
                 $urlData['scheme'],
